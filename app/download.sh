786de0b6c686c054254111940174e64f047e4dba
David Sperling <dsperling@smithmicro.com>
2023-03-25 13:01:00 -0700

    docs fix

--- app/download.sh
+++ app/download.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-set -e
+# download data from www.nominatim.org
+# For OSM files, please use:
+# https://github.com/openmaptiles/openmaptiles-tools
 
-MIRROR_URI=https://download.geofabrik.de
+set -e
 
 download() {
   DOWNLOAD_FILENAME=$(basename "$1")
@@ -21,15 +23,6 @@ download() {
 
 help() {
   echo "download [-n|-m|-w|-u|-k|-g|-h] [long options see below]"
-  echo "  -f|--africa -          Africa OSM file"
-  echo "  -t|--antarctica -      Antarctica OSM file"
-  echo "  -a|--asia -            Asia OSM file"
-  echo "  -o|--oceania -         Australia and Oceania OSM file"
-  echo "  -c|--central-america - Central America OSM file"
-  echo "  -e|--europe -          Europe OSM file"
-  echo "  -n|--north-america -   North America OSM file"
-  echo "  -s|--south-america -   South America OSM file"
-  echo "  -m|--monaco -          Monaco OSM File - a small test"
   echo "  -w|--wiki -      Wikimedia file to improve search"
   echo "  -u|--us-postal - US postal code data"
   echo "  -k|--uk-postal - UK postal code data"
@@ -42,54 +35,18 @@ help() {
 
 while [[ $# -gt 0 ]]; do
   case $1 in
-    -f|--africa)
-      download $MIRROR_URI/africa-latest.osm.pbf
-      shift
-      ;;
-    -t|--antarctica)
-      download $MIRROR_URI/antarctica-latest.osm.pbf
-      shift
-      ;;
-    -a|--asia)
-      download $MIRROR_URI/asia-latest.osm.pbf
-      shift
-      ;;
-    -o|--oceania)
-      download $MIRROR_URI/australia-oceania-latest.osm.pbf
-      shift
-      ;;
-    -c|--central-america)
-      download $MIRROR_URI/central-america-latest.osm.pbf
-      shift
-      ;;
-    -e|--europe)
-      download $MIRROR_URI/europe-latest.osm.pbf
-      shift
-      ;;
-    -n|--north-america)
-      download $MIRROR_URI/north-america-latest.osm.pbf
-      shift
-      ;;
-    -s|--south-america)
-      download $MIRROR_URI/south-america-latest.osm.pbf
-      shift
-      ;;
-    -m|--monaco)
-      download $MIRROR_URI/europe/monaco-latest.osm.pbf
-      shift
-      ;;
     -w|--wiki)
       # improve the quality of search results
       download https://www.nominatim.org/data/wikimedia-importance.sql.gz
       shift
       ;;
     -u|--us-postal)
-      # improve postal code search
+      # improve US postal code search
       download https://www.nominatim.org/data/us_postcodes.csv.gz
       shift
       ;;
     -k|--uk-postal)
-      # improve postal code search
+      # improve UK postal code search
       download https://www.nominatim.org/data/gb_postcodes.csv.gz
       shift
       ;;
